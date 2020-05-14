971e46d1ee374dc8a7e56e5abcac9d303c387389
Jordan Pannell <jpannell1@sheffield.ac.uk>
2020-05-14 11:18:17 +0100

    Changed the "checking gauges" file so that it analyses recent simulations I ran for the peak impulse distributions.

--- Scripts/checking_gauges.py
+++ Scripts/checking_gauges.py
@@ -21,116 +21,142 @@ params = {'font.family':'serif',
 plt.rcParams.update(params)
 
 
-# main_dataset_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_dataset\*.txt")
-# original_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\original_PE4_100g_theta80_z055_16\*.txt")
-# original_file = original_file[4::]
-# latest_1500_r3_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res3\*.txt")
-# latest_1500_r4_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res4\*.txt")
-# latest_1500_r5_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res5\*.txt")
-# latest_var_r4_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\VAR_ZL40mm_res4\*.txt")
+main_dataset_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_dataset\*.txt")
+original_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\original_PE4_100g_theta80_z055_16\*.txt")
+original_file = original_file[4::]
+latest_1500_r3_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res3\*.txt")
+latest_1500_r4_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res4\*.txt")
+latest_1500_r5_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res5\*.txt")
+latest_var_r4_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\VAR_ZL40mm_res4\*.txt")
 
 
 
-# main_dataset = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_dataset\*gtable",1)
-# original = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\original_PE4_100g_theta80_z055_16\*gtable",1)
-# original = original[4::]
-# latest_1500_r3 = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res3\*gtable",1)
-# latest_1500_r4 = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res4\*gtable",1)
-# latest_1500_r5 = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res5\*gtable",1)
-# latest_var_r4 = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\VAR_ZL40mm_res4\*gtable",1)
+main_dataset = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_dataset\*gtable",1)
+original = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\original_PE4_100g_theta80_z055_16\*gtable",1)
+original = original[4::]
+latest_1500_r3 = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res3\*gtable",1)
+latest_1500_r4 = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res4\*gtable",1)
+latest_1500_r5 = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\1500mm_ZL100mm_res5\*gtable",1)
+latest_var_r4 = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\Sphere\main_z055_16_latest\VAR_ZL40mm_res4\*gtable",1)
 
 
-# main_dataset = np.asarray([main_dataset[i][:,7] for i in range(len(main_dataset))]).T
-# original = np.asarray([original[i][:,7] for i in range(len(original))]).T
-# latest_1500_r3 = np.asarray([latest_1500_r3[i][:,7] for i in range(len(latest_1500_r3))]).T
-# latest_1500_r4 = np.asarray([latest_1500_r4[i][:,7] for i in range(len(latest_1500_r4))]).T
-# latest_1500_r5 = np.asarray([latest_1500_r5[i][:,7] for i in range(len(latest_1500_r5))]).T
-# latest_var_r4 = np.asarray([latest_var_r4[i][:,7] for i in range(len(latest_var_r4))]).T
+main_dataset = np.asarray([main_dataset[i][:,7] for i in range(len(main_dataset))]).T
+original = np.asarray([original[i][:,7] for i in range(len(original))]).T
+latest_1500_r3 = np.asarray([latest_1500_r3[i][:,7] for i in range(len(latest_1500_r3))]).T
+latest_1500_r4 = np.asarray([latest_1500_r4[i][:,7] for i in range(len(latest_1500_r4))]).T
+latest_1500_r5 = np.asarray([latest_1500_r5[i][:,7] for i in range(len(latest_1500_r5))]).T
+latest_var_r4 = np.asarray([latest_var_r4[i][:,7] for i in range(len(latest_var_r4))]).T
+
+
+fig, ax = plt.subplots(1,1)
+ax.plot(main_dataset)
+ax.set_title("main dataset - 0.02m, res3 - 2.5mm")
+fig, ax = plt.subplots(1,1)
+ax.plot(original)
+ax.set_title("original - 0.1m, res4 - 6.25mm")
+fig, ax = plt.subplots(1,1)
+ax.plot(latest_var_r4)
+ax.set_title("latest domain = 1.2xgaugelength - 0.04m, res4 - 2.5mm")
+fig, ax = plt.subplots(1,1)
+ax.plot(latest_1500_r3)
+ax.set_title("latest_1500mm - 0.1m, res3 - 12.5mm")
+fig, ax = plt.subplots(1,1)
+ax.plot(latest_1500_r4)
+ax.set_title("latest_1500mm - 0.1m, res4 - 6.25mm")
+fig, ax = plt.subplots(1,1)
+ax.plot(latest_1500_r5)
+ax.set_title("latest_1500mm - 0.1m, re5 - 3.125mm")
+
+fig, ax = plt.subplots(1,1)
+ax.plot(latest_var_r4, 'c', label = 'Variable domain, 40mm, res4')
+ax.plot(latest_1500_r3, 'g', label = 'Large domain, 100mm, res3')
+ax.plot(latest_1500_r4, 'r', label = 'Large domain, 100mm, res4')
+ax.plot(latest_1500_r5, 'm', label = 'Large domain, 100mm, res5')
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, labels, loc='upper right', prop={'size':6})
+
+
+fig, [ax0, ax1] = plt.subplots(2,1)
+ax0.plot(np.linspace(0,80,300), original, 'k')
+ax0.plot(np.linspace(0,80,200), main_dataset, 'g')
+ax0.plot(np.linspace(0,80,200), latest_1500_r3, 'b')
+ax0.plot(np.linspace(0,80,200), latest_1500_r4, 'y')
+ax0.plot(np.linspace(0,80,200), latest_1500_r5,  'm')
+ax0.plot(np.linspace(0,80,200), latest_var_r4, 'c')
+ax0.set_xlabel('Theta')
+ax1.set_ylabel('Peak specific impulse')
+
+#ax1.plot(np.linspace(0,80,200), main_dataset/main_dataset.max(0), 'g')
+ax1.plot(np.linspace(0,80,200), latest_1500_r3/latest_1500_r3.max(0), 'b')
+ax1.plot(np.linspace(0,80,200), latest_1500_r4/latest_1500_r4.max(0), 'y')
+ax1.plot(np.linspace(0,80,200), latest_1500_r5/latest_1500_r5.max(0),  'm')
+ax1.plot(np.linspace(0,80,200), latest_var_r4/latest_var_r4.max(0), 'c')
+ax1.set_xlabel('Theta')
+ax1.set_ylabel('Peak specific impulse ratio')
+labels = ['1500mm^3 domain, 100mm ZL, res3', '1500mm^3 domain, 100mm ZL, res4', '1500mm^3 domain, 100mm ZL, res5',
+          'variable domain, 40mm ZL, res4',]
+colors = ['b', 'y', 'm', 'c']
+lines = [Line2D([0], [0], color=c, linewidth=0.5) for c in colors]
+ax1.legend(lines,labels)
+
+
+main_dataset_I = [Impulse_CFD(main_dataset[:,i], 1, 80, np.linspace(0,80,200)) for i in range(len(main_dataset_file))]
+original_I = [Impulse_CFD(original[:,i], 1, 80, np.linspace(0,80,300)) for i in range(len(original_file))]
+latest_1500_r3_I = [Impulse_CFD(latest_1500_r3[:,i], 1, 80, np.linspace(0,80,200)) for i in range(len(latest_1500_r3_file))]
+latest_1500_r4_I = [Impulse_CFD(latest_1500_r4[:,i], 1, 80, np.linspace(0,80,200)) for i in range(len(latest_1500_r4_file))]
+latest_1500_r5_I = [Impulse_CFD(latest_1500_r5[:,i], 1, 80, np.linspace(0,80,200)) for i in range(len(latest_1500_r5_file))]
+latest_var_r4_I = [Impulse_CFD(latest_var_r4[:,i], 1, 80, np.linspace(0,80,200)) for i in range(len(latest_var_r4_file))]
 
 
 
+# #--------------------------------------------------------------------------------
+# main_dataset_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\mesh_strategy\z0_055\*.txt")
+# main_dataset = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\mesh_strategy\z0_055\*_gauges",1)
+# Apollo_gauges = [main_dataset[i] for i in [0,1,4,5]]
 
-# fig, [ax0, ax1] = plt.subplots(2,1)
-# ax0.plot(np.linspace(0,80,300), original, 'k')
-# ax0.plot(np.linspace(0,80,200), main_dataset, 'g')
-# ax0.plot(np.linspace(0,80,200), latest_1500_r3, 'b')
-# ax0.plot(np.linspace(0,80,200), latest_1500_r4, 'y')
-# ax0.plot(np.linspace(0,80,200), latest_1500_r5,  'm')
-# ax0.plot(np.linspace(0,80,200), latest_var_r4, 'c')
-# ax0.set_xlabel('Theta')
-# ax1.set_ylabel('Peak specific impulse')
-
-# #ax1.plot(np.linspace(0,80,200), main_dataset/main_dataset.max(0), 'g')
-# ax1.plot(np.linspace(0,80,200), latest_1500_r3/latest_1500_r3.max(0), 'b')
-# ax1.plot(np.linspace(0,80,200), latest_1500_r4/latest_1500_r4.max(0), 'y')
-# ax1.plot(np.linspace(0,80,200), latest_1500_r5/latest_1500_r5.max(0),  'm')
-# ax1.plot(np.linspace(0,80,200), latest_var_r4/latest_var_r4.max(0), 'c')
-# ax1.set_xlabel('Theta')
-# ax1.set_ylabel('Peak specific impulse ratio')
-# labels = ['1500mm^3 domain, 100mm ZL, res3', '1500mm^3 domain, 100mm ZL, res4', '1500mm^3 domain, 100mm ZL, res5',
-#           'variable domain, 40mm ZL, res4',]
-# colors = ['b', 'y', 'm', 'c']
-# lines = [Line2D([0], [0], color=c, linewidth=0.5) for c in colors]
-# ax1.legend(lines,labels)
-
-
-# main_dataset_I = [Impulse_CFD(main_dataset[:,i], 1, 80, np.linspace(0,80,200)) for i in range(len(main_dataset_file))]
-# original_I = [Impulse_CFD(original[:,i], 1, 80, np.linspace(0,80,300)) for i in range(len(original_file))]
-# latest_1500_r3_I = [Impulse_CFD(latest_1500_r3[:,i], 1, 80, np.linspace(0,80,200)) for i in range(len(latest_1500_r3_file))]
-# latest_1500_r4_I = [Impulse_CFD(latest_1500_r4[:,i], 1, 80, np.linspace(0,80,200)) for i in range(len(latest_1500_r4_file))]
-# latest_1500_r5_I = [Impulse_CFD(latest_1500_r5[:,i], 1, 80, np.linspace(0,80,200)) for i in range(len(latest_1500_r5_file))]
-# latest_var_r4_I = [Impulse_CFD(latest_var_r4[:,i], 1, 80, np.linspace(0,80,200)) for i in range(len(latest_var_r4_file))]
-
-
-
-#--------------------------------------------------------------------------------
-main_dataset_file = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\mesh_strategy\z0_055\*.txt")
-main_dataset = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\mesh_strategy\z0_055\*_gauges",1)
-Apollo_gauges = [main_dataset[i] for i in [0,1,4,5]]
-
-#Quick test to see if enough time in simulation
-for i in range(len(Apollo_gauges)):
-    fig00, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2,2)
-    ax1.set_xlabel(i)
-    #theta = 0
-    ax1.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,1]) #OP
-    ax2.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,201])#imp
+# #Quick test to see if enough time in simulation
+# for i in range(len(Apollo_gauges)):
+#     fig00, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2,2)
+#     ax1.set_xlabel(i)
+#     #theta = 0
+#     ax1.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,1]) #OP
+#     ax2.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,201])#imp
     
-    #theta = 80
-    ax3.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,200])#OP
-    ax4.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,400])#imp
+#     #theta = 80
+#     ax3.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,200])#OP
+#     ax4.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,400])#imp
 
 
 
-#-----------------------
-val_80mm_chosenmesh_gauges = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\mesh_strategy\80mm_validation\*_gauges",1)
-val_80mm_chosenmesh = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\mesh_strategy\80mm_validation\*_gtable",1)
-Apollo_gauges = val_80mm_chosenmesh_gauges
+# #-----------------------
+# val_80mm_chosenmesh_gauges = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\mesh_strategy\80mm_validation\*_gauges",1)
+# val_80mm_chosenmesh = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Impulse Distribution Curve Modelling\Paper_1\mesh_strategy\80mm_validation\*_gtable",1)
+# Apollo_gauges = val_80mm_chosenmesh_gauges
 
-Apollo_gtable_z80mm_first = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Near Field Sims\Sims\Latest\80mm_with_afterburn\*gtable",1)
-z80mm_first_theta = np.rad2deg(np.arctan2(Apollo_gtable_z80mm_first[0][:,2], 0.08))
+# Apollo_gtable_z80mm_first = pre.FileAddressList(os.environ['USERPROFILE'] + r"\Google Drive\Apollo Sims\Near Field Sims\Sims\Latest\80mm_with_afterburn\*gtable",1)
+# z80mm_first_theta = np.rad2deg(np.arctan2(Apollo_gtable_z80mm_first[0][:,2], 0.08))
 
-#Quick test to see if enough time in simulation
-for i in range(len(Apollo_gauges)):
-    fig00, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2,2)
-    ax1.set_xlabel(i)
-    #theta = 0
-    ax1.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,1]) #OP
-    ax2.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,201])#imp
+# #Quick test to see if enough time in simulation
+# for i in range(len(Apollo_gauges)):
+#     fig00, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2,2)
+#     ax1.set_xlabel(i)
+#     #theta = 0
+#     ax1.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,1]) #OP
+#     ax2.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,201])#imp
     
-    #theta = 80
-    ax3.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,200])#OP
-    ax4.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,400])#imp
+#     #theta = 80
+#     ax3.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,200])#OP
+#     ax4.plot(Apollo_gauges[i][:,0],Apollo_gauges[i][:,400])#imp
     
     
-#peak impulse distribution for first 0.3ms
-term = 0.3e-3
-z80mm_chosenmesh_fin = int(np.argwhere(Apollo_gauges[0][:,0]>term)[0][0])
-fig8, [ax, ax0] = plt.subplots(1,2)
-fig8.set_size_inches(5, 3)
-ax.set_xlabel('theta (degrees)')
-ax.set_ylabel('peak specific scaled impulse')
-ax.plot(np.linspace(0,80,200), np.max(Apollo_gauges[0][0:z80mm_chosenmesh_fin,201:], axis = 0), 'k', label = '3.125mm')
+# #peak impulse distribution for first 0.3ms
+# term = 0.3e-3
+# z80mm_chosenmesh_fin = int(np.argwhere(Apollo_gauges[0][:,0]>term)[0][0])
+# fig8, [ax, ax0] = plt.subplots(1,2)
+# fig8.set_size_inches(5, 3)
+# ax.set_xlabel('theta (degrees)')
+# ax.set_ylabel('peak specific scaled impulse')
+# ax.plot(np.linspace(0,80,200), np.max(Apollo_gauges[0][0:z80mm_chosenmesh_fin,201:], axis = 0), 'k', label = '3.125mm')
 
 # ax0.set_xlabel('theta (degrees)')
 # ax0.set_ylabel('peak impulse ratio of maximum')
@@ -141,3 +167,8 @@ ax.plot(np.linspace(0,80,200), np.max(Apollo_gauges[0][0:z80mm_chosenmesh_fin,20
 # handles, labels = ax0.get_legend_handles_labels()
 # ax0.legend(handles, labels, loc='center', bbox_to_anchor=(0.7, 0.80), prop={'size':6})
 # plt.tight_layout()
+
+
+
+
+#Checking impulse distributions....
